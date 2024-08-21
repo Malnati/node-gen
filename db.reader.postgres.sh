584ae2b0f326dcee7dae380fb06e5547b71791e5
Malnati <ricardomalnati@gmail.com>
2024-08-21 14:13:57 -0300

    Update output directory path and file paths for component generation

--- db.reader.postgres.sh
+++ db.reader.postgres.sh
@@ -1,7 +1,7 @@
+#!/bin/bash
+
 rm -rf ./build
-
 cp -r ./static/. ./build/
-
 npm run build && npx ts-node src/main.ts --host "34.134.67.65" --port "5432" --database "biud_log" --user "biud_log" --password $1 --outputDir "./build" --outputFile 'db.reader.postgres.json'
 
 # npx ts-node src/db.reader.postgres.ts
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@
 
 import { DbReader } from './db.reader.postgres';
 import { ConfigUtil } from './utils/ConfigUtil';
+import { execSync } from 'child_process';
+import * as readline from 'readline';
 
 const dbConfig = ConfigUtil.getConfig(); // Obtém as configurações do banco de dados
 
@@ -17,3 +19,51 @@ console.log(`Output File: ${dbConfig.outputFile}`);
 
 const dbReader = new DbReader(dbConfig); // Passa as configurações para o DbReader
 dbReader.getSchemaInfo(); // Executa o método para obter o schema e gerar o arquivo
+
+// Função para perguntar ao usuário quais componentes deseja gerar
+function askQuestion(query: string): Promise<string> {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  return new Promise((resolve) => rl.question(query, (ans) => {
+    rl.close();
+    resolve(ans);
+  }));
+}
+
+async function main() {
+  const componentsToGenerate = await askQuestion(
+    'Especifique quais componentes gerar (entities, services, interfaces, controllers, dtos, modules, app-module, main, env, package.json, readme): '
+  );
+
+  const components = componentsToGenerate.split(',').map(component => component.trim().toLowerCase());
+
+  // Mapeamento das opções para os comandos correspondentes
+  const commandsMap: { [key: string]: string } = {
+    'entities': `npx ts-node src/typeorm-entity-generator.ts`,
+    'services': `npx ts-node src/service-generator.ts`,
+    'interfaces': `npx ts-node src/interface-generator.ts`,
+    'controllers': `npx ts-node src/controller-generator.ts`,
+    'dtos': `npx ts-node src/dto-generator.ts`,
+    'modules': `npx ts-node src/module-generator.ts`,
+    'app-module': `npx ts-node src/app-module-generator.ts`,
+    'main': `npx ts-node src/main-generator.ts`,
+    'env': `npx ts-node src/env-generator.ts`,
+    'package.json': `npx ts-node src/package-json-generator.ts`,
+    'readme': `npx ts-node src/readme-generator.ts`,
+  };
+
+  for (const component of components) {
+    const command = commandsMap[component];
+    if (command) {
+      console.log(`Executando comando para ${component}: ${command}`);
+      execSync(command, { stdio: 'inherit' });
+    } else {
+      console.log(`Componente ${component} não reconhecido.`);
+    }
+  }
+}
+
+main();
