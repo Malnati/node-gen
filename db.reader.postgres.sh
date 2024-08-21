3be23b1d30862c4ac7d3a6a75eb69f615145f62a
Malnati <ricardomalnati@gmail.com>
2024-08-21 18:57:37 -0300

    chore: Update README.md

--- db.reader.postgres.sh
+++ db.reader.postgres.sh
@@ -14,16 +14,3 @@ npm run build && \
                     --outputDir "./build" \
                     --components "entities, services, interfaces, controllers, dtos, modules, app-module, main, env, package.json, readme"
 
-# ['entities'|'services'|'interfaces'|'controllers'|'dtos'|'modules'|'app-module'|'main'|'env'|'package.json'|'readme'];
-# npx ts-node src/db.reader.postgres.ts
-# npx ts-node src/typeorm-entity-generator.ts
-# npx ts-node src/service-generator.ts
-# npx ts-node src/interface-generator.ts
-# npx ts-node src/controller-generator.ts
-# npx ts-node src/dto-generator.ts
-# npx ts-node src/module-generator.ts
-# npx ts-node src/app-module-generator.ts
-# npx ts-node src/main-generator.ts
-# npx ts-node src/env-generator.ts
-# npx ts-node src/package-json-generator.ts
-# npx ts-node src/readme-generator.ts
