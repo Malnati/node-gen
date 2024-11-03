59e657ca8a2e4e2d5c812f096c793ab766de53dc
Malnati <ricardomalnati@gmail.com>
2024-11-03 18:24:05 -0300

    feat: update Docker configuration for new microservice structure and improve PostgreSQL setup

--- docker-start.sh
+++ docker-start.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-# Define a variável de ambiente DATABASE_URL
-case $1 in
-  local)
-    DATABASE_URL="postgresql://postgres:postgres@127.0.0.1:5432/app?serverVersion=15&charset=utf8"
-    ;;
-  desenv)
-    DATABASE_URL="postgresql://postgres:postgres@desenv:5432/app?serverVersion=15&charset=utf8"
-    ;;
-esac
+cd .docker
 
-# Inicia o Docker Compose
-docker-compose up
+docker-compose -p "node_gen" down --volumes --remove-orphans
+
+docker compose -p "node_gen" up --build -d
+
+cd ..
