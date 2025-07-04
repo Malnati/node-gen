49029e465a13ca4b5c4e5b20ebf0279f8705d9e5
Ricardo Malnati <35036961+Malnati@users.noreply.github.com>
2025-07-04 19:39:21 -0300

    Improve README template

--- templates/readme.template.ts
+++ templates/readme.template.ts
@@ -1,5 +1,11 @@
+<%# Nome do micro-serviço %>
 # Repositório do micro-serviço {{appName}}
 
+![Node.js Version](https://img.shields.io/badge/node-%3E=20-blue)
+![License](https://img.shields.io/badge/license-MIT-green)
+![Build Status](https://img.shields.io/badge/build-passing-brightgreen)
+
+<%# Nome do banco gerado %>
 ## {{appName}} Database
 
 Este documento descreve a estrutura do banco de dados e os passos para sua criação, incluindo a definição das tabelas, colunas, comentários, funções e triggers. A motivação para a criação deste documento é fornecer um guia detalhado para a configuração do banco de dados, garantindo que todas as etapas sejam seguidas corretamente para uma implementação consistente. Este documento também explica as vantagens de utilizar colunas como `external_id`, funções e triggers diretamente no banco de dados, comparado com a implementação no backend.
@@ -21,4 +27,30 @@ Este documento descreve a estrutura do banco de dados e os passos para sua cria�
 
 ![Diagrama do Banco de Dados](public/diagram.png)
 
+<%# Conteúdo gerado para cada tabela %>
 {{sections}}
+
+---
+
+## Como usar
+
+### Via CLI
+
+1. Instale as dependências:
+   ```bash
+   npm install
+   ```
+2. Inicie o projeto:
+   ```bash
+   npm start
+   ```
+
+### Via Docker
+
+```bash
+docker-compose up
+```
+
+### Swagger/API
+
+A documentação estará disponível em `http://localhost:3000/swagger`.
