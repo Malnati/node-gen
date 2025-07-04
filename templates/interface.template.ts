2fbf620196d86bade9397169503f81f13be1cdf0
Ricardo Malnati <35036961+Malnati@users.noreply.github.com>
2025-07-04 19:48:31 -0300

    Implement issue #123 (#130)
    feat: improve interface template

--- templates/interface.template.ts
+++ templates/interface.template.ts
@@ -1,3 +1,10 @@
+/**
+ * Interfaces de transferência de dados para {{entityName}}.
+ *
+ * `I{{entityName}}QueryDTO` descreve o formato de retorno das consultas,
+ * enquanto `I{{entityName}}PersistDTO` define os campos aceitos para
+ * criação ou atualização da entidade.
+ */
 {{queryDto}}
 
 {{persistDto}}
